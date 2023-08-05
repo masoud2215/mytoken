bbe055c7650c20e1a6ea96880ed7253d88e04f57
masoud2215 <50657846+masoud2215@users.noreply.github.com>
2023-08-05 16:08:46 +0330

    Update index.js
    Update index

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xc344c1cac5d9b1f53571157359da9d56e4bf2901";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
